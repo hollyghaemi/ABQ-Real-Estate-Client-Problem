001983fec3c24d27c679a710541ec38ddefce441
Holly Ghaemi <34724022+hollyghaemi@users.noreply.github.com>
2019-03-19 08:37:26 -0500

    Add files via upload

---
+++ RS21_sentimentanalysis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 20:09:54 2019
+
+@author: hallehghaemi
+"""
+
+#%% Read in data
+
+os.chdir("/Users/hallehghaemi/Desktop/RS21")
+twitter_master = pd.read_excel('Twitter_141103.xlsx')
